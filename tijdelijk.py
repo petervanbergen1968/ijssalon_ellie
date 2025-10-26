7dce5e171e58a428d420f31c958b6489da7cfc92
Peter van Bergen <osberg@ziggo.nl>
2025-10-26 15:17:47 +0100

    funties toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,19 +1,27 @@
-prijzen_dictionary = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-}
-aanbieding = prijzen_dictionary["aardbei"] * 0.8
-reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}")
-reclame_tekst2 = reclame_tekst[:63]
-woorden = reclame_tekst2.split()
+from helper import decoreer
 
-for el in woorden:
-    if len(el) >= 5:
-        el = el.upper()
-    else:
-        el = el.lower()
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
 
-    for letter in el:
-        print(letter)
-    print()
+    aanbieding = prijzen["aardbei"] + prijzen["vanille"] * 0.8
+
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding}"
+
+    reclame_tekst2 = reclame_tekst[:63]
+
+    reclame_tekst3 = reclame_tekst2.upper()
+
+    reclame_tekst4 = reclame_tekst3.split()
+
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
